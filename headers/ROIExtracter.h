0b5ffc0dd4b06623a76a81eff4f83da814be3580
gd123yjy <1224624112@qq.com>
2019-05-18 14:10:37 +0800

    baseline to extract palm ROI

---
+++ headers/ROIExtracter.h
@@ -0,0 +1,86 @@
+#include <opencv2/core.hpp>
+#include <math.h>
+
+using namespace std;
+
+class ROIExtracter
+{
+private:
+    /* data */
+    int *finger_roots;
+
+public:
+    ROIExtracter(int finger_roots[]);
+    ~ROIExtracter();
+    cv::Point **extract(int);
+};
+
+ROIExtracter::ROIExtracter(int finger_roots[])
+{
+    this->finger_roots = finger_roots;
+}
+
+ROIExtracter::~ROIExtracter()
+{
+}
+
+cv::Point **ROIExtracter::extract(int roi_size)
+{
+    // int i = 1;
+    // const int *const j = &i;
+    // const int *const *p = &j;
+
+    cv::Point **result = new cv::Point *[1];
+    result[0] = new cv::Point[4];
+    cv::Mat A = cv::Mat::zeros(cv::Size2d(2, 2), CV_32F);
+    cv::Mat B = cv::Mat::zeros(cv::Size2d(4, 2), CV_32F);
+    cv::Mat C = cv::Mat::zeros(cv::Size2d(4, 2), CV_32F);
+    cv::Mat result_mat;
+    int a = this->finger_roots[0];
+    int b = this->finger_roots[1];
+    int c = this->finger_roots[4];
+    int d = this->finger_roots[5];
+    float m = sqrt((a - c) * (a - c) + (b - d) * (b - d));
+
+    A.at<float>(0, 0) = (a - c) / m;
+    A.at<float>(0, 1) = (b - d) / m;
+    A.at<float>(1, 0) = (d - b) / m;
+    A.at<float>(1, 1) = (a - c) / m;
+
+    B.at<float>(0, 0) = roi_size / 2.0;
+    B.at<float>(0, 1) = roi_size / 2.0;
+    B.at<float>(0, 2) = -roi_size / 2.0;
+    B.at<float>(0, 3) = -roi_size / 2.0;
+    B.at<float>(1, 1) = roi_size;
+    B.at<float>(1, 2) = roi_size;
+
+    C.at<float>(0, 0) = (a + c) / 2;
+    C.at<float>(0, 1) = (a + c) / 2;
+    C.at<float>(0, 2) = (a + c) / 2;
+    C.at<float>(0, 3) = (a + c) / 2;
+    C.at<float>(1, 0) = (b + d) / 2;
+    C.at<float>(1, 1) = (b + d) / 2;
+    C.at<float>(1, 2) = (b + d) / 2;
+    C.at<float>(1, 3) = (b + d) / 2;
+
+    //cv::gemm(A, B,1,C,1, result_mat);
+    result_mat = A.inv() * B + C;
+
+    result[0][0] = cv::Point(result_mat.at<float>(0, 0), result_mat.at<float>(1, 0));
+    result[0][1] = cv::Point(result_mat.at<float>(0, 1), result_mat.at<float>(1, 1));
+    result[0][2] = cv::Point(result_mat.at<float>(0, 2), result_mat.at<float>(1, 2));
+    result[0][3] = cv::Point(result_mat.at<float>(0, 3), result_mat.at<float>(1, 3));
+
+    // print(A);
+    // printf("\n");
+    // print(B);
+    // printf("\n");
+    // print(C);
+    // printf("\n");
+    // print(A.inv() * B);
+    // printf("\n");
+    // print(result_mat);
+    // printf("\n");
+
+    return result;
+}
---
+++ src/ROIExtract.cpp
@@ -0,0 +1,96 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+#include <fstream>
+#include <iostream>
+
+#include "ROIExtracter.h"
+
+using namespace std;
+
+vector<string> split(const string &str, const string &delim);
+
+/*
+input: fingers coordinates
+output: ROI, represented by four points
+*/
+int main()
+{
+    const string img_load_base = "/home/yjy/dataset/palmprint_dectection/LHand/palmvein/";
+    const string fg_load_base = "/home/yjy/PycharmProjects/firstAI/Finger_roots_Eager/datasets/LHand/palmprint_trainval/";
+    const string roi_save_base = "/home/yjy/PycharmProjects/firstAI/Finger_roots_Eager/datasets/LHand/palmprint_trainval/";
+    const string img_save_base = "/home/yjy/dataset/palmprint_dectection/LHand/palmvein_ROI/";
+    cv::Mat img;
+    const int pt_num = 4;
+    const int ROI_size = 224;
+    char buff[100000];
+    char out_buff[1000];
+    int finger_roots[6] = {0};
+
+    ifstream txt(fg_load_base + "figCon_nn.txt"); //"figCon.txt"
+    ofstream txt_roi(fg_load_base + "ROI.txt");
+    txt.read(buff, 100000);
+    string figCon(buff);
+    string outrow = "";
+    vector<string>
+        rows = split(figCon, "\n");
+
+    for (int i = 0; i < rows.size(); i++)
+    {
+        string row = rows[i];
+        vector<string> data = split(row, " ");
+        string filename = data[0].substr(0, 17);
+        for (int j = 0; j < 6; j++)
+        {
+            finger_roots[j] = stoi(data[j + 1]);
+        }
+        ROIExtracter roie(finger_roots);
+
+        try
+        {
+            img = cv::imread(img_load_base + filename);
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << e.what() << '\n';
+            continue;
+        }
+
+        const cv::Point *const *result = roie.extract(ROI_size);
+        cv::polylines(img, result, &pt_num, 1, true, cv::Scalar(0, 255, 0));
+        cv::imwrite(img_save_base + filename + ".jpg", img);
+
+        sprintf(out_buff, "%s %d %d %d %d %d %d %d %d\n", filename.c_str(), result[0][0].x, result[0][0].y, result[0][1].x, result[0][1].y, result[0][2].x, result[0][2].y, result[0][3].x, result[0][3].y);
+        txt_roi << out_buff;
+
+        delete[] result; // bad destruction
+    }
+
+    txt_roi.close();
+
+    return 0;
+}
+
+vector<string> split(const string &str, const string &delim)
+{
+    vector<string> res;
+    if ("" == str)
+        return res;
+    //先将要切割的字符串从string类型转换为char*类型
+    char *strs = new char[str.length() + 1]; //不要忘了
+    strcpy(strs, str.c_str());
+
+    char *d = new char[delim.length() + 1];
+    strcpy(d, delim.c_str());
+
+    char *p = strtok(strs, d);
+    while (p)
+    {
+        string s = p;     //分割得到的字符串转换为string类型
+        res.push_back(s); //存入结果数组
+        p = strtok(NULL, d);
+    }
+
+    return res;
+}
